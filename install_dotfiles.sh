bb187d86b17c93f68bc020c32eb40868c1dda40f
Spencer Russell <spencer.f.russell@gmail.com>
2013-06-13 00:36:17 -0400

    adds zsh setup to install_dotfiles

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -3,6 +3,23 @@
 # this script assumes that the config files are all contained in a
 # directory named "dotfiles" in your home directory
 
+PLATFORM=`uname`
+
+echo "Installing zsh..."
+if [ $PLATFORM == 'Darwin' ]; then
+    brew install zsh
+    grep /usr/local/bin/zsh /etc/shells || sudo echo "/usr/local/bin/zsh" >> /etc/shells
+    chsh -s /usr/local/bin/zsh
+
+elif [ $PLATFORM == 'Linux' ] && [ -e /usr/bin/apt-get ]; then
+    sudo apt-get install zsh
+    grep /usr/bin/zsh /etc/shells || sudo echo "/usr/bin/zsh" >> /etc/shells
+    chsh -s /usr/bin/zsh
+else
+    echo "Unsupported system, please add setup in install_dotfiles.sh"
+    exit 1
+fi
+
 cd ~
 
 # create symbolic links for config files
@@ -17,11 +34,12 @@ rm ~/.git
 
 cd dotfiles
 # pull submodules
-git submodule init
-git submodule update
+git submodule update --init
 
 for conffile in zlogin zlogout zprofile zshenv
 do
     echo "Linking ${conffile}"
     ln -s ~/.zprezto/runcoms/${conffile} ~/.${conffile}
 done
+
+echo "Dotfile setup complete. You may need to log out completely for new settings to take effect"
