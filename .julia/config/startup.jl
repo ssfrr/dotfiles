62e23ccc8bb5e4c0a147318bce66037c24e54a12
Spencer Russell <spencer.f.russell@gmail.com>
2019-04-02 12:33:43 -0400

    aesthetic tweaks to the julia startup

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,5 +1,8 @@
 ENV["JULIA_PKG_DEVDIR"] = "$(ENV["HOME"])/Dropbox/juliadev"
 
+# erase the prompt so the following log message look nice
+print("\r")
+
 using Pkg
 if isfile("Project.toml")
     # auto-activate project in current directory
@@ -23,3 +26,7 @@ try
 catch ex
     @warn "Could not load OhMyREPL: $ex"
 end
+
+# redisplay the prompt so we know we're done
+using REPL: REPL
+REPL.LineEdit.refresh_line(Base.active_repl.mistate)
